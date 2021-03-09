ad35b1834f4fe55535df12f4d4a5f7010e16fa97
zhongjinye <610505041@qq.com>
2021-03-09 14:42:39 +0800

    use self delimit helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
 
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
